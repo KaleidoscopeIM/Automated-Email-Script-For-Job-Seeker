ec3f45f61b6ed31f08cc0db4119c8d00ea8a45bc
Gautam Saini <iamgautamsaini@gmail.com>
2020-08-31 09:58:29 -0400

    updates

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var cron = require('node-cron');
 
-//cron.schedule('0 8 * * *', () => {
-cron.schedule('0 13 * * *', () => { // 9 local == 13 on server
+cron.schedule('* * * * *', () => {
     var spawn = require("child_process").spawn;
-    //var process = spawn('python', ["./sendMailRecruiter.py"]);
-    var process = spawn('python3', ["./sendMailRecruiter.py"]);
+    var process = spawn('python', ["./sendMailRecruiter.py"]);
     process.stdout.on('data', function(data) {
         console.log(data.toString());
     })
 });
+
+
+
+// cron.schedule('0 13 * * *', () => { // 9 local == 13 on server
+//     var spawn = require("child_process").spawn;
+//     var process = spawn('python3', ["./sendMailRecruiter.py"]);
+//     process.stdout.on('data', function(data) {
+//         console.log(data.toString());
+//     })
+// });
